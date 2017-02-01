1ecf1109269ce0d71c6a9b451996e8a7bf457bf8
Toru Maesaka <toru@torumk.com>
2017-01-31 19:24:10 -0800

    Add a name validation to place_detail model

--- app/models/place_detail.rb
+++ app/models/place_detail.rb
@@ -1,3 +1,5 @@
 class PlaceDetail < ApplicationRecord
   belongs_to :place
+
+  validates :name, presence: true
 end
--- test/models/place_detail_test.rb
+++ test/models/place_detail_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
-class PlaceDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe PlaceDetail do
+  describe '#invalid?' do
+    it 'fails to validate without a name' do
+      place_detail = build :place_detail, name: nil
+
+      assert place_detail.invalid?
+      assert_includes place_detail.errors, :name
+    end
+  end
 end
