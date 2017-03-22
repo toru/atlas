eddaf1c76795b2c4756a9527567207e642c561ee
Toru Maesaka <toru@torumk.com>
2017-03-21 21:34:34 -0700

    Transparently set the id on checkin creation

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,2 +1,19 @@
 class Checkin < ApplicationRecord
+  MAX_ID_LEN = 16
+
+  before_create :set_id
+
+  private
+
+  def set_id
+    retries_allowed = 3
+
+    loop do
+      self.id = SecureRandom.hex[0..(MAX_ID_LEN-1)]
+      break unless Checkin.exists?(id)
+
+      retries_allowed -= 1
+      break unless retries_allowed > 0
+    end
+  end
 end
