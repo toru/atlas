4088fc4f7a6136d9a8ac2804a76becc7ec25fe3e
Toru Maesaka <toru@torumk.com>
2017-04-11 09:07:47 -0700

    Strip out the time from created_at if show_time is false

--- app/serializers/checkin_serializer.rb
+++ app/serializers/checkin_serializer.rb
@@ -2,4 +2,8 @@ class CheckinSerializer < ActiveModel::Serializer
   attributes :id, :comment, :created_at
 
   has_one :place
+
+  def created_at
+    object.show_time? ? object.created_at : object.created_at.to_date
+  end
 end
