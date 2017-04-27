ee05b615c88b5708c23e640dd22ecc888155f2a4
Toru Maesaka <toru@torumk.com>
2017-04-26 19:01:28 -0700

    Add a `links` object to place representation

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,9 +1,16 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :created_at, :category, :links
 
   belongs_to :category
 
   def id
     object.external_id
+  end
+
+  def links
+    # TODO(toru): Stop hard coding the host
+    [{ self: place_url(object.external_id, host: 'api.torumk.com') }]
   end
 end
